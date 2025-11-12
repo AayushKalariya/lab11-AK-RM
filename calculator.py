8c333dad1a70fdce8f11dfa4eac70a3ac0987665
Aayush Kalariya <103155943+AayushKalariya@users.noreply.github.com>
2025-11-12 17:13:40 -0500

    Goat

---
+++ calculator.py
@@ -0,0 +1,23 @@
+import math 
+
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b/a
+
+def log(a, b): 
+    if b == 0:
+        raise ValueError
+    return log(b,a)
+
+def exp(a,b):
+    return a**b
